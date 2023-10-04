5af0e6f214661d286bbff74637060a7b39d727a6
Chirag Vaghasiya <chirag.vaghasiya@aspiresoftserv.in>
2023-10-04 10:17:06 +0530

    df

--- aspl_currency_update/models/res_config_settings.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api, _
-
-
-class ResConfigSettings(models.TransientModel):
-    _inherit = "res.config.settings"
-    _name = "res.config.settings"
-
-    api_key = fields.Char(string="API Key", related='company_id.api_key', readonly=False)
--- aspl_currency_update/models/res_currency_rate.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields, api, _
-
-
-class ResCurrencyRate(models.Model):
-    _inherit = "res.currency.rate"
-    
-    current_company_currency = fields.Many2one('res.currency', string="Base Currency")
